a0493e6085308118746f1a9ac50c8581a7828ab1
quentinwolf <18410863+quentinwolf@users.noreply.github.com>
2024-07-15 09:57:48 -0600

    Create config.py
    Initial commit, moving configuration options to separate file for simplicity

---
+++ config.py
@@ -0,0 +1,14 @@
+debugmode = False
+verbosemode = False
+
+colored_console_output = False  # Requires https://pypi.org/project/termcolor
+
+auto_accept_mod_invites = False
+
+# Config Validation Errors are always PM'ed regardless of being True or False
+send_pm_on_wiki_config_update = True
+
+discord_bot_notifications = False
+discord_webhook_url = "https://discord.com/api/webhooks/YOUR_DISCORD_WEBHOOK"
+
+logs_dir = "logs/"
