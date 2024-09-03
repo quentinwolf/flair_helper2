78805176ad9925f66cbde71c4a9585a642c0180e
quentinwolf <18410863+quentinwolf@users.noreply.github.com>
2024-09-02 20:41:16 -0600

    Update config.py
    Added Telegram Bot Control as well as 'allow_ban_and_nuke'

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
+# Flair Helper 2 config.py
+
 debugmode = False
 verbosemode = False
 
-colored_console_output = False  # Requires https://pypi.org/project/termcolor
+telegram_bot_control = False #Change to True to enable Telegram Bot Functionality
+telegram_TOKEN = 'YourBot:TokenIDHere'
+telegram_admin_ids = [123456789, 98765421] #ID's of Telegram Users you want to allow interactions with your bot.  If they aren't on this list, interactions with the bot will be ignored.
+
+colored_console_output = True #Requires https://pypi.org/project/termcolor
 
 auto_accept_mod_invites = False
+
+allow_ban_and_nuke = False
 
 # Config Validation Errors are always PM'ed regardless of being True or False
 send_pm_on_wiki_config_update = True
